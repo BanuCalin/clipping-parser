8b58444900d5ef369c1f28b5e9fd98db9d43215c
Banu Calin <banucaliniulian@gmail.com>
2021-10-27 23:41:09 +0300

    Move code to module and refactor

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,80 @@ extern crate clap;
 
 use clap::{Arg, App};
 use std::fs::File;
-// use std::fs::OpenOptions;
-use std::io::{BufReader, Read};
-// use regex::Regex;
-use std::collections::HashMap;
+use std::io::{BufReader, Read, Error};
+
+mod Parser {
+    use std::collections::HashMap;
+
+    #[derive(Debug)]
+    pub struct ClipParser {
+        hm: HashMap<String, HashMap<String, Vec<(String, String)>>>,
+    }
+
+    pub fn get_clip_parser() -> ClipParser {
+        return ClipParser {
+            hm: HashMap::new(),
+        };
+    }
+
+    impl ClipParser {
+        pub fn parse_file_string(&mut self, data: &str) {
+            for entry in data.split("==========") {
+                if entry.len() < 10 { continue; } // There might be a "\r\n" after the last "=" sequence
+                let mut entry_lines = entry.lines().filter(|x| x.len() > 1);
+                let title_author : Vec<&str> = entry_lines.next().unwrap().split(&['(', ')'][..]).collect();
+                let title = String::from(title_author[0].trim());
+                let author = String::from(title_author[1].trim());
+                let entry_data = String::from(entry_lines.next().unwrap());
+                let quote = String::from(entry_lines.next().unwrap());
+                match self.hm.get_mut(&author) {
+                    None => {
+                        let mut quotes: Vec<(String, String)> = Vec::new();
+                        quotes.push((quote, entry_data));
+                        let mut books_hm: HashMap<String, Vec<(String, String)>> = HashMap::new();
+                        books_hm.insert(title, quotes);
+                        self.hm.insert(author, books_hm);
+                    }
+                    Some(books_hm) => {
+                        match books_hm.get_mut(&title) {
+                            None => {
+                                let mut quotes: Vec<(String, String)> = Vec::new();
+                                quotes.push((quote, entry_data));
+                                books_hm.insert(title, quotes);
+                            }
+                            Some(quotes) => {
+                                quotes.push((quote, entry_data));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        pub fn get_authors(&self) -> Vec<&String> {
+            return self.hm.keys().collect();
+        }
+    }
+}
+
+fn read_input_file(file_path: &str) -> Result<String, Error> {
+    let mut buf = String::new();
+    let file = match File::open(file_path) {
+        Ok(file) => file,
+        Err(err) => {
+            println!("Error opening input file: {}", err);
+            return Err(err);
+        }
+    };
+    match BufReader::new(file).read_to_string(&mut buf) {
+        Ok(_) => (),
+        Err(err) => {
+            println!("Error reading input file: {}", err);
+            return Err(err)
+        }
+    };
+    return Ok(buf);
+}
 
 fn main() {
     let matches = App::new("Kindle Clipping Parser")
@@ -20,56 +90,14 @@ fn main() {
             .takes_value(true))
         .get_matches();
 
-    let mut buf = String::new();
-    let mut authors_hm : HashMap<&str, HashMap<&str, Vec<(&str, &str)>>> = HashMap::new();
-
+    let mut parser = Parser::get_clip_parser();
     if let Some(file_path) = matches.value_of("file") {
         println!("Input file: {}", file_path);
-        let file = match File::open(file_path) {
-            Ok(file) => file,
-            Err(err) => {
-                println!("Error opening input file: {}", err);
-                std::process::exit(1);
-            }
-        };
-        match BufReader::new(file).read_to_string(&mut buf) {
-            Ok(_) => (),
-            Err(err) => {
-                println!("Error reading input file: {}", err);
-                std::process::exit(1);
-            }
-        };
-
-        for entry in buf.split("==========") {
-            if entry.len() < 10 { continue; } // There might be a "\r\n" after the last "=" sequence
-            let mut entry_lines = entry.lines().filter(|x| x.len() > 1);
-            let title_author : Vec<&str> = entry_lines.next().unwrap().split(&['(', ')'][..]).collect();
-            let title = title_author[0].trim();
-            let author = title_author[1].trim();
-            let entry_data = entry_lines.next().unwrap();
-            let quote = entry_lines.next().unwrap();
-            // authors_hm.entry(author).or_insert();
-            match authors_hm.get_mut(author) {
-                None => {
-                    let mut quotes: Vec<(&str, &str)> = Vec::new();
-                    quotes.push((quote, entry_data));
-                    let mut books_hm: HashMap<&str, Vec<(&str, &str)>> = HashMap::new();
-                    books_hm.insert(title, quotes);
-                    authors_hm.insert(author, books_hm);
-                }
-                Some(books_hm) => {
-                    match books_hm.get_mut(title) {
-                        None => {
-                            let mut quotes: Vec<(&str, &str)> = Vec::new();
-                            quotes.push((quote, entry_data));
-                            books_hm.insert(title, quotes);
-                        }
-                        Some(quotes) => {
-                            quotes.push((quote, entry_data));
-                        }
-                    }
-                }
-            }
+        match read_input_file(file_path) {
+            Ok(buf) => parser.parse_file_string(&buf),
+            Err(_) => std::process::exit(1),
         }
     }
+
+    // #### Scratchpad
 }
